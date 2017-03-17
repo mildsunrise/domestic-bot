cb372f1988e3d3a70be973a4b7dd0eb0cf795548
Alba Mendez <me@alba.sh>
2017-03-17 14:08:36 +0000

    Bot

---
+++ server.js
@@ -0,0 +1,48 @@
+var botgram = require("botgram");
+var dgram = require("dgram");
+var bot = botgram("243637402:AAGnwv4qt2qAntMmaSXC6pcHJIHPdNa5eG4");
+var Timer = require("./timer");
+
+var reply = bot.reply(-207667727);
+var secReply = bot.reply(97438879);
+
+var backend = dgram.createSocket("udp4");
+backend.bind(5897);
+backend.on("message", (msg, rinfo) => {
+  try {
+    msg = JSON.parse(msg.toString("utf-8"));
+  } catch (e) {return console.error(e);}
+  if (typeof msg.from === "string" && Object.hasOwnProperty.call(handlers, msg.from))
+    handlers[msg.from](msg);
+});
+
+var vocoreHeartbeat = new Timer(120 * 1000); // 2:00
+var vocoreOnline = true;
+function setVocoreOnline(online) {
+  if (online === vocoreOnline) return;
+  vocoreOnline = online;
+  if (online) {
+    secReply.silent().markdown("Vocore online again");
+  } else {
+    secReply.markdown("Vocore **offline**!");
+  }
+}
+
+vocoreHeartbeat.on("fire", () => setVocoreOnline(false));
+vocoreHeartbeat.reset();
+
+var vocoreRepeat = new Timer(4 * 1000);
+var vocoreRepeating = false;
+vocoreRepeat.on("fire", () => { vocoreRepeating = false; });
+
+var handlers = {
+  "vocore": (msg) => {
+    setVocoreOnline(true);
+    vocoreHeartbeat.reset();
+    if (msg.type === "hit" && !vocoreRepeating) {
+      reply.markdown("🛎 Llaman al timbre");
+      vocoreRepeating = true;
+      vocoreRepeat.reset();
+    }
+  },
+};
---
+++ timer.js
@@ -0,0 +1,44 @@
+var util = require("util");
+var EventEmitter = require("events");
+
+function Timer(timeout) { 
+  this.timeout = timeout;
+}
+util.inherits(Timer, EventEmitter);
+
+// Core
+
+/* Clear timer if set. */
+Timer.prototype.clear = function clear() {
+  if (this.set) clearTimeout(this.set);
+  this.set = null;
+};
+
+/* Schedule timer, clear first if set. */
+Timer.prototype.reset = function reset(t) {
+  this.clear();                        
+  if (t === undefined) t = this.timeout;
+  this.set = setTimeout(function callback() {
+    this.set = null;
+    this.emit("fire");
+  }.bind(this), t);
+};
+
+// Others
+
+/* Schedule timer if not set. */
+Timer.prototype.set = function set(t) {
+  if (!this.set) this.reset(t);
+};
+
+/* Reschedule timer if set. */
+Timer.prototype.restart = function (t) {
+  if (this.set) this.reset(t);
+};
+
+/* Fire (and clear) timer in the next tick. */
+Timer.prototype.fire = function fire() {
+  this.reset(0);
+};
+
+module.exports = Timer;
