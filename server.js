4c53d7593ba41e8e088e7f75739c0621f8b21e62
Alba Mendez <me@alba.sh>
2017-05-13 18:19:04 +0200

    Change to MQTT based

--- server.js
+++ server.js
@@ -1,48 +1,56 @@
 var botgram = require("botgram");
-var dgram = require("dgram");
+var mqtt = require("mqtt");
 var bot = botgram("243637402:AAGnwv4qt2qAntMmaSXC6pcHJIHPdNa5eG4");
 var Timer = require("./timer");
 
 var reply = bot.reply(-207667727);
 var secReply = bot.reply(97438879);
 
-var backend = dgram.createSocket("udp4");
-backend.bind(5897);
-backend.on("message", (msg, rinfo) => {
-  try {
-    msg = JSON.parse(msg.toString("utf-8"));
-  } catch (e) {return console.error(e);}
-  if (typeof msg.from === "string" && Object.hasOwnProperty.call(handlers, msg.from))
-    handlers[msg.from](msg);
+var client = mqtt.connect("mqtt://local:711f462fba54@localhost");
+client.on("connect", function () {
+  client.subscribe("vocore-timbre/ring-time");
+  client.subscribe("vocore-timbre/online");
+  ignore = true;
+  setTimeout(() => { ignore = false; }, 1000);
 });
 
-var vocoreHeartbeat = new Timer(210 * 1000); // 3:30
-var vocoreOnline = true;
-function setVocoreOnline(online) {
-  if (online === vocoreOnline) return;
-  vocoreOnline = online;
-  if (online) {
-    secReply.silent().markdown("Vocore online again");
-  } else {
-    secReply.markdown("Vocore **offline**!");
-  }
+var currentOnline = true;
+var ignore = true;
+
+function setCurrentOnline(online) {
+  if (online == currentOnline) return; 
+  if (online) secReply.silent().markdown("Vocore online again.");
+  else secReply.markdown("Vocore offline!");
+  currentOnline = online;
 }
 
-vocoreHeartbeat.on("fire", () => setVocoreOnline(false));
-vocoreHeartbeat.reset();
+var onlineTimer = new Timer(1000);
+onlineTimer.on("fire", () => {
+  setCurrentOnline(false);
+});
 
-var vocoreRepeat = new Timer(4 * 1000);
-var vocoreRepeating = false;
-vocoreRepeat.on("fire", () => { vocoreRepeating = false; });
+client.on("message", function (topic, msg) {
+  try {
+    msg = msg.toString("utf-8");
+  } catch (e) {
+    return;
+  }
 
-var handlers = {
-  "vocore": (msg) => {
-    setVocoreOnline(true);
-    vocoreHeartbeat.reset();
-    if (msg.type === "hit" && !vocoreRepeating) {
+  if (topic === "vocore-timbre/ring-time") {
+    if (ignore) return;
+    var n = parseInt(msg, 10);
+    if (isNaN(n) || n < Date.now()/1000 - 5)
+      reply.markdown("Llaman al timbre, fuera de rango: " + n + " (" + (Math.floor(Date.now()/1000)-n) + ")");
+    else
       reply.markdown("🛎 Llaman al timbre");
-      vocoreRepeating = true;
-      vocoreRepeat.reset();
+  }
+  if (topic === "vocore-timbre/online") {
+    if (msg !== "true" && msg !== "false") return;
+    var online = JSON.parse(msg);
+    if (!online) onlineTimer.reset();
+    else {
+      onlineTimer.clear();
+      setCurrentOnline(true);
     }
-  },
-};
+  }
+});
